53b0a7943f2b2e8622fcd98e2d26884d9e85bed2
alpetrushkin <104225655+alpetrushkin@users.noreply.github.com>
2022-12-21 15:59:33 +0300

    Session Storage

--- src/App.tsx
+++ src/App.tsx
@@ -8,37 +8,15 @@ function App() {
       setValue(value + 1)
    }
 
-   const setLocalStorageHandler = () => {
-      localStorage.setItem('counterValue', JSON.stringify(value))
-      localStorage.setItem('counterValue + 1', JSON.stringify(value + 1))
-   }
-
-   const getLocalStorageHandler = () => {
-      let valueAsString = localStorage.getItem('counterValue')
-      if (valueAsString) {
-         let newValue = JSON.parse(valueAsString)
-         setValue(newValue)
-      }
-   }
-
-   const clearLocalStorageHandler = () => {
-      localStorage.clear()
-      setValue(0)
-   }
-
-   
-   const removeFromItemLocalStorageHandler = () => {
-     localStorage.removeItem('counterValue + 1')
+   const setToLocalStorage = () => {
+      sessionStorage.setItem('counterValue', JSON.stringify(value))
    }
 
    return (
       <div className="App">
          <h3>{value}</h3>
          <button onClick={onClickIncHandler}>INC</button>
-         <button onClick={setLocalStorageHandler}>setLocalStorage</button>
-         <button onClick={getLocalStorageHandler}>getLocalStorage</button>
-         <button onClick={clearLocalStorageHandler}>clearLocalStorage</button>
-         <button onClick={removeFromItemLocalStorageHandler}>removeFromItemLocalStorage</button>
+         <button onClick={setToLocalStorage}>setToLocalStorage</button>
       </div>
    );
 }
